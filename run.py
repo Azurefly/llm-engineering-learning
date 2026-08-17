784b9db7e7909e90922468857f565c0aa432fb27
SuPan <azure_dream@foxmail.com>
2026-08-17 15:17:18 +0800

    start V5 adaptive application

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.main_v4:app",
+        "app.main_v5:app",
         host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "8765")),
         reload=os.getenv("RELOAD", "0") == "1",
